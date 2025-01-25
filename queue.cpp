0f9c1a802567a48c53a278ea609b0bfc573211e1
mokhtar <mostafamokhtar743@gmail.com>
2025-01-25 20:48:14 +0200

    put the template into the files

---
+++ queue.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define El_Mokh ios_base::sync_with_stdio(false); cin.tie(NULL), cout.tie(NULL);
+
+void sol(){
+    /////* Solution */////
+    
+    
+    
+    //////////////////////
+}
+
+int main(){
+     
+    El_Mokh
+    int t = 1;
+    cin >> t;
+    while(t--)
+        sol();
+}
